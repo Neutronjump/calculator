f735a29fb2547c4ef4a99261d7158848767fea3a
Neutronjump <theamazingneutronjump@gmail.com>
2026-09-13 15:03:38 -0400

    Declare functions to get operator and operands
    getOperator reads the display and returns the operator.
    getLeftOperand reads the display and returns the left operand.
    getRightOperand reads the display and returns the right operand.
    These all run when the equals button is pressed.
    (operate's multiplication and divison cases were changed to match the
    calculator's symbols for each operation)

--- script.js
+++ script.js
@@ -2,13 +2,45 @@ calculator();
 
 function calculator(){
     let calculator = document.querySelector(".calculator");
-    let display = document.querySelector(".calculator-display p")
+    let display = document.querySelector(".calculator-display p");
+    let equalsButton = document.querySelector(".equals");
 
     displayCalc(calculator, display);
     clearDisplay(calculator, display);
 
+    equalsButton.addEventListener("click", (e) => {
+        getOperator(display);
+        getLeftOperand(display);
+        getRightOperand(display);
+    })
 }
-    
+
+function getOperator(display) {
+    const symbolsToRemove = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "="];
+    let displayArr = display.textContent.split("");
+    let operator = displayArr.filter((item) => !(symbolsToRemove.includes(item)))[0];
+    return operator;
+}
+
+function getLeftOperand(display) {
+    let displayContent = display.textContent.split("");
+    let operator =  getOperator(display);
+    let leftOperand = displayContent
+    .slice(0,displayContent.indexOf(operator))
+    .join("");
+    return leftOperand;
+}
+
+function getRightOperand(display) {
+    let displayContent = display.textContent.split("");
+    let operator =  getOperator(display);
+    let rightOperand = displayContent
+    .slice(displayContent.indexOf(operator)+1)
+    .join("");
+    console.log(rightOperand);
+    return rightOperand;
+}
+
 function clearDisplay(calculator, display) {
     calculator.addEventListener("click", (e) => {
         if (e.target.closest("button") && e.target.textContent === "C") {
@@ -32,9 +64,9 @@ function operate(operator, num1, num2) {
             return add(num1, num2);
         case "-":
             return subtract(num1, num2);
-        case "*":
+        case "×":
             return multiply(num1, num2);
-        case "/":
+        case "÷":
             return divide(num1, num2);
     }
 }
